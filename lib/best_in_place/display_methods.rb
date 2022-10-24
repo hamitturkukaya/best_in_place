0cbc196f5a6563c7db4e25a4d2b2659e4441d954
H. Türkü Kaya <htkaya90@gmail.com>
2022-10-24 14:44:18 +0300

    Ruby 3 updates

--- lib/best_in_place/display_methods.rb
+++ lib/best_in_place/display_methods.rb
@@ -9,7 +9,7 @@ module BestInPlace
             { display_as: object.send(opts[:method]) }.to_json
           when :helper
             value = if opts[:helper_options]
-                      BestInPlace::ViewHelpers.send(opts[:method], object.send(opts[:attr]), opts[:helper_options])
+                      BestInPlace::ViewHelpers.send(opts[:method], object.send(opts[:attr]), **opts[:helper_options])
                     else
                       BestInPlace::ViewHelpers.send(opts[:method], object.send(opts[:attr]))
                     end
@@ -30,15 +30,15 @@ module BestInPlace
     end
 
     def add_model_method(klass, attr, display_as)
-      model_attributes(klass)[attr.to_s] = Renderer.new method: display_as.to_sym, type: :model
+      model_attributes(klass)[attr.to_s] = Renderer.new({method: display_as.to_sym, type: :model})
     end
 
     def add_helper_method(klass, attr, helper_method, helper_options = nil)
-      model_attributes(klass)[attr.to_s] = Renderer.new method: helper_method.to_sym, type: :helper, attr: attr, helper_options: helper_options
+      model_attributes(klass)[attr.to_s] = Renderer.new({method: helper_method.to_sym, type: :helper, attr: attr, helper_options: helper_options})
     end
 
     def add_helper_proc(klass, attr, helper_proc)
-      model_attributes(klass)[attr.to_s] = Renderer.new type: :proc, attr: attr, proc: helper_proc
+      model_attributes(klass)[attr.to_s] = Renderer.new({type: :proc, attr: attr, proc: helper_proc})
     end
 
     def model_attributes(klass)
